4bf4474f29295bec115b953335526dd5888c0ca0
termie <github@anarkystic.com>
2011-12-10 12:33:48 -0800

    initial brain

---
+++ policy.py
@@ -0,0 +1,72 @@
+
+
+def can_haz(match_list, target_dict, credentials_dict):
+  """Check the authz of some rules against credentials.
+
+  Match lists look like:
+
+    ('rule:compute:get_volume',)
+
+  or
+
+    ('role:compute_admin',
+     ('tenant_id:%(tenant_id)s', 'role:compute_sysadmin'))
+
+
+  Target dicts contain as much information as we can about the object being
+  operated on.
+
+  Credentials dicts contain as much information as we can about the user
+  performing the action.
+
+  """
+  b = Brain()
+  if not b.check(match_list, target_dict, credentials_dict):
+    raise Exception('Not Allowed')
+
+
+
+class Brain(object):
+  # class level on purpose, the brain is global
+  rules = {}
+
+  def __init__(self, rules=None):
+    if rules is not None:
+      self.__class__.rules = rules
+
+  def add_rule(self, key, match):
+    self.rules[key] = match
+
+  def check(self, match_list, target_dict, cred_dict):
+    for and_list in match_list:
+      matched = False
+      for match in and_list:
+        # check each rule and if any fail return false
+        if match.startswith('rule:'):
+          new_match_list = self.rules.get(match[5:])
+          rv = self.check(new_match_list, target_dict, cred_dict)
+          if not rv:
+            matched = False
+            break
+        else:
+          rv = self._check(match, target_dict, cred_dict)
+          if not rv:
+            matched = False
+            break
+        matched = True
+
+      # all AND matches passed
+      if matched:
+        return True
+
+    # no OR rules matched
+    return False
+
+  def _check(self, match, target_dict, cred_dict):
+    # TODO(termie): do dict inspection via dot syntax
+    match = match % target_dict
+    if match not in cred_dict:
+      return False
+    return True
+
+
